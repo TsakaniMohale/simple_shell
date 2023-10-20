fbc1290b789cf12b0b5a43779be182c272af4e14
Tsakani Mohale <tsakanimohale4@gmail.com>
2023-10-19 18:53:14 -0700

    simple shell

--- main.c
+++ main.c
@@ -2,12 +2,61 @@
 #include <stdbool.h>
 
 /**
+ * signal_handler - Handles interrupt Ctrl+C signal
+ * @signal: The signal
+ */
+
+void signal_handler(int signal)
+{
+	(void)signal;
+	write(STDOUT_FILENO, "\n#cisfun$ ", 10);
+	kill(getpid(), SIGCONT);
+}
+
+/**
  * is_interactive - Returns associated file descriptor
  * Return: input(0) file descriptor
  */
 bool is_interactive(void)
 {
-	return (isatty(STDIN_FILENO));
+	return (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO));
+}
+
+/**
+ * get_line - Get the the user inpute
+ * @command: Arguments array
+ * Return: The command read
+ */
+char *get_line(char **command)
+{
+	char *input = NULL;
+	size_t length = 0;
+	ssize_t n_chars;
+
+	signal(SIGINT, signal_handler);
+
+	n_chars = getline(&input, &length, stdin);
+	if (n_chars == -1)
+	{
+		free(input);
+		input = NULL;
+		if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		exit(0);
+	}
+	if (strcmp(input, "\n") == 0)
+	{
+		free(input);
+		input = NULL;
+		get_line(command);
+	}
+	if (input[n_chars - 1] == '\n')
+	{
+		input[n_chars - 1] = '\0';
+	}
+	return (input);
 }
 
 /**
@@ -19,25 +68,16 @@ bool is_interactive(void)
 
 int main(int argc, char **argv)
 {
-	ssize_t n_chars;
-	size_t n;
 	char *command = NULL;
 	const char *tokens_array[100] = {NULL, NULL};
 	(void)argc;
 
 	for ( ; 1; )
 	{
-		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
+		if (is_interactive())
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		n_chars = getline(&command, &n, stdin);
-		if (n_chars < 0)
-			break;
-		if (command[n_chars - 1] == '\n')
-		{
-			command[n_chars - 1] = '\0';
-			n_chars = n_chars - 1;
-		}
+		command = get_line((char **)tokens_array);
 
 		tokenization(command, tokens_array);
 		exec(tokens_array, argv);
